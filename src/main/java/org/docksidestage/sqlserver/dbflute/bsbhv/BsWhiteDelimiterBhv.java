94d2d7206388c2254e2e092ffdfff0a4b78a0775
jflute <dbflute@gmail.com>
2014-11-26 17:38:06 +0900

    Release DBFlute-1.1.0-RC1

--- src/main/java/org/docksidestage/sqlserver/dbflute/bsbhv/BsWhiteDelimiterBhv.java
+++ src/main/java/org/docksidestage/sqlserver/dbflute/bsbhv/BsWhiteDelimiterBhv.java
@@ -554,11 +554,7 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
      * <span style="color: #3F7E5E">//whiteDelimiter.set...;</span>
      * <span style="color: #3F7E5E">// if exclusive control, the value of concurrency column is required</span>
      * whiteDelimiter.<span style="color: #CC4747">setVersionNo</span>(value);
-     * try {
-     *     <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">update</span>(whiteDelimiter);
-     * } catch (EntityAlreadyUpdatedException e) { <span style="color: #3F7E5E">// if concurrent update</span>
-     *     ...
-     * }
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">update</span>(whiteDelimiter);
      * </pre>
      * @param whiteDelimiter The entity of update. (NotNull, PrimaryKeyNotNull)
      * @throws EntityAlreadyDeletedException When the entity has already been deleted. (not found)
@@ -667,17 +663,15 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
      * This method uses executeBatch() of java.sql.PreparedStatement.
      * <pre>
      * <span style="color: #3F7E5E">// e.g. update two columns only</span>
-     * whiteDelimiterBhv.<span style="color: #CC4747">batchUpdate</span>(whiteDelimiterList, new SpecifyQuery&lt;WhiteDelimiterCB&gt;() {
-     *     public void specify(WhiteDelimiterCB cb) { <span style="color: #3F7E5E">// the two only updated</span>
-     *         cb.specify().<span style="color: #CC4747">columnFooStatusCode()</span>; <span style="color: #3F7E5E">// should be modified in any entities</span>
-     *         cb.specify().<span style="color: #CC4747">columnBarDate()</span>; <span style="color: #3F7E5E">// should be modified in any entities</span>
-     *     }
+     * whiteDelimiterBhv.<span style="color: #CC4747">batchUpdate</span>(whiteDelimiterList, <span style="color: #553000">colCB</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #3F7E5E">// the two only updated</span>
+     *     <span style="color: #553000">colCB</span>.specify().<span style="color: #CC4747">columnFooStatusCode()</span>; <span style="color: #3F7E5E">// should be modified in any entities</span>
+     *     <span style="color: #553000">colCB</span>.specify().<span style="color: #CC4747">columnBarDate()</span>; <span style="color: #3F7E5E">// should be modified in any entities</span>
      * });
      * <span style="color: #3F7E5E">// e.g. update every column in the table</span>
-     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">batchUpdate</span>(whiteDelimiterList, new SpecifyQuery&lt;WhiteDelimiterCB&gt;() {
-     *     public void specify(WhiteDelimiterCB cb) { <span style="color: #3F7E5E">// all columns are updated</span>
-     *         cb.specify().<span style="color: #CC4747">columnEveryColumn()</span>; <span style="color: #3F7E5E">// no check of modified properties</span>
-     *     }
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">batchUpdate</span>(whiteDelimiterList, <span style="color: #553000">colCB</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #3F7E5E">// all columns are updated</span>
+     *     <span style="color: #553000">colCB</span>.specify().<span style="color: #CC4747">columnEveryColumn()</span>; <span style="color: #3F7E5E">// no check of modified properties</span>
      * });
      * </pre>
      * <p>You can specify update columns used on set clause of update statement.
@@ -751,9 +745,9 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
      * <span style="color: #3F7E5E">// you don't need to set a value of concurrency column</span>
      * <span style="color: #3F7E5E">// (auto-increment for version number is valid though non-exclusive control)</span>
      * <span style="color: #3F7E5E">//whiteDelimiter.setVersionNo(value);</span>
-     * WhiteDelimiterCB cb = <span style="color: #70226C">new</span> WhiteDelimiterCB();
-     * cb.query().setFoo...(value);
-     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">queryUpdate</span>(whiteDelimiter, cb);
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">queryUpdate</span>(whiteDelimiter, <span style="color: #553000">cb</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">cb</span>.query().setFoo...
+     * });
      * </pre>
      * @param whiteDelimiter The entity that contains update values. (NotNull, PrimaryKeyNullAllowed)
      * @param cbLambda The callback for condition-bean of WhiteDelimiter. (NotNull)
@@ -793,9 +787,9 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
     /**
      * Delete the several entities by query. (NonExclusiveControl)
      * <pre>
-     * WhiteDelimiterCB cb = new WhiteDelimiterCB();
-     * cb.query().setFoo...(value);
-     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">queryDelete</span>(whiteDelimiter, cb);
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">queryDelete</span>(whiteDelimiter, <span style="color: #553000">cb</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">cb</span>.query().setFoo...
+     * });
      * </pre>
      * @param cbLambda The callback for condition-bean of WhiteDelimiter. (NotNull)
      * @return The deleted count.
@@ -835,10 +829,10 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
      * <span style="color: #3F7E5E">// if auto-increment, you don't need to set the PK value</span>
      * whiteDelimiter.setFoo...(value);
      * whiteDelimiter.setBar...(value);
-     * InsertOption&lt;WhiteDelimiterCB&gt; option = new InsertOption&lt;WhiteDelimiterCB&gt;();
-     * <span style="color: #3F7E5E">// you can insert by your values for common columns</span>
-     * option.disableCommonColumnAutoSetup();
-     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingInsert</span>(whiteDelimiter, option);
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingInsert</span>(whiteDelimiter, <span style="color: #553000">op</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #3F7E5E">// you can insert by your values for common columns</span>
+     *     <span style="color: #553000">op</span>.disableCommonColumnAutoSetup();
+     * });
      * ... = whiteDelimiter.getPK...(); <span style="color: #3F7E5E">// if auto-increment, you can get the value after</span>
      * </pre>
      * @param whiteDelimiter The entity of insert. (NotNull, PrimaryKeyNullAllowed: when auto-increment)
@@ -859,18 +853,12 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
      * whiteDelimiter.setOther...(value); <span style="color: #3F7E5E">// you should set only modified columns</span>
      * <span style="color: #3F7E5E">// if exclusive control, the value of concurrency column is required</span>
      * whiteDelimiter.<span style="color: #CC4747">setVersionNo</span>(value);
-     * <span style="color: #70226C">try</span> {
-     *     <span style="color: #3F7E5E">// you can update by self calculation values</span>
-     *     UpdateOption&lt;WhiteDelimiterCB&gt; option = new UpdateOption&lt;WhiteDelimiterCB&gt;();
-     *     option.self(new SpecifyQuery&lt;WhiteDelimiterCB&gt;() {
-     *         public void specify(WhiteDelimiterCB cb) {
-     *             cb.specify().<span style="color: #CC4747">columnXxxCount()</span>;
-     *         }
+     * <span style="color: #3F7E5E">// you can update by self calculation values</span>
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingUpdate</span>(whiteDelimiter, <span style="color: #553000">op</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">op</span>.self(<span style="color: #553000">cb</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *         <span style="color: #553000">cb</span>.specify().<span style="color: #CC4747">columnXxxCount()</span>;
      *     }).plus(1); <span style="color: #3F7E5E">// XXX_COUNT = XXX_COUNT + 1</span>
-     *     <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingUpdate</span>(whiteDelimiter, option);
-     * } <span style="color: #70226C">catch</span> (EntityAlreadyUpdatedException e) { <span style="color: #3F7E5E">// if concurrent update</span>
-     *     ...
-     * }
+     * });
      * </pre>
      * @param whiteDelimiter The entity of update. (NotNull, PrimaryKeyNotNull)
      * @param opLambda The callback for option of update for varying requests. (NotNull)
@@ -979,15 +967,13 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
      * <span style="color: #3F7E5E">// you don't need to set a value of concurrency column</span>
      * <span style="color: #3F7E5E">// (auto-increment for version number is valid though non-exclusive control)</span>
      * <span style="color: #3F7E5E">//whiteDelimiter.setVersionNo(value);</span>
-     * WhiteDelimiterCB cb = new WhiteDelimiterCB();
-     * cb.query().setFoo...(value);
-     * UpdateOption&lt;WhiteDelimiterCB&gt; option = <span style="color: #70226C">new</span> UpdateOption&lt;WhiteDelimiterCB&gt;();
-     * option.self(new SpecifyQuery&lt;WhiteDelimiterCB&gt;() {
-     *     public void specify(WhiteDelimiterCB cb) {
-     *         cb.specify().<span style="color: #CC4747">columnFooCount()</span>;
-     *     }
-     * }).plus(1); <span style="color: #3F7E5E">// FOO_COUNT = FOO_COUNT + 1</span>
-     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingQueryUpdate</span>(whiteDelimiter, cb, option);
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingQueryUpdate</span>(whiteDelimiter, <span style="color: #553000">cb</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">cb</span>.query().setFoo...
+     * }, <span style="color: #553000">op</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">op</span>.self(<span style="color: #553000">colCB</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *         <span style="color: #553000">colCB</span>.specify().<span style="color: #CC4747">columnFooCount()</span>;
+     *     }).plus(1); <span style="color: #3F7E5E">// FOO_COUNT = FOO_COUNT + 1</span>
+     * });
      * </pre>
      * @param whiteDelimiter The entity that contains update values. (NotNull) {PrimaryKeyNotRequired}
      * @param cbLambda The callback for condition-bean of WhiteDelimiter. (NotNull)
@@ -1015,13 +1001,11 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
      * <span style="color: #3F7E5E">//whiteDelimiter.setVersionNo(value);</span>
      * WhiteDelimiterCB cb = <span style="color: #70226C">new</span> WhiteDelimiterCB();
      * cb.query().setFoo...(value);
-     * UpdateOption&lt;WhiteDelimiterCB&gt; option = <span style="color: #70226C">new</span> UpdateOption&lt;WhiteDelimiterCB&gt;();
-     * option.self(new SpecifyQuery&lt;WhiteDelimiterCB&gt;() {
-     *     public void specify(WhiteDelimiterCB cb) {
-     *         cb.specify().<span style="color: #CC4747">columnFooCount()</span>;
-     *     }
-     * }).plus(1); <span style="color: #3F7E5E">// FOO_COUNT = FOO_COUNT + 1</span>
-     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingQueryUpdate</span>(whiteDelimiter, cb, option);
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">varyingQueryUpdate</span>(whiteDelimiter, cb, <span style="color: #553000">op</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">op</span>.self(<span style="color: #553000">colCB</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *         <span style="color: #553000">colCB</span>.specify().<span style="color: #CC4747">columnFooCount()</span>;
+     *     }).plus(1); <span style="color: #3F7E5E">// FOO_COUNT = FOO_COUNT + 1</span>
+     * });
      * </pre>
      * @param whiteDelimiter The entity that contains update values. (NotNull) {PrimaryKeyNotRequired}
      * @param cb The condition-bean of WhiteDelimiter. (NotNull)
@@ -1036,7 +1020,14 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
     /**
      * Delete the several entities by query with varying requests non-strictly. <br>
      * For example, allowNonQueryDelete(). <br>
-     * Other specifications are same as batchUpdateNonstrict(entityList).
+     * Other specifications are same as queryDelete(cb).
+     * <pre>
+     * <span style="color: #0000C0">whiteDelimiterBhv</span>.<span style="color: #CC4747">queryDelete</span>(whiteDelimiter, <span style="color: #553000">cb</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">cb</span>.query().setFoo...
+     * }, <span style="color: #553000">op</span> <span style="color: #90226C; font-weight: bold"><span style="font-size: 120%">-</span>&gt;</span> {
+     *     <span style="color: #553000">op</span>...
+     * });
+     * </pre>
      * @param cbLambda The callback for condition-bean of WhiteDelimiter. (NotNull)
      * @param opLambda The callback for option of delete for varying requests. (NotNull)
      * @return The deleted count.
@@ -1049,7 +1040,7 @@ public abstract class BsWhiteDelimiterBhv extends AbstractBehaviorWritable<White
     /**
      * Delete the several entities by query with varying requests non-strictly. <br>
      * For example, allowNonQueryDelete(). <br>
-     * Other specifications are same as batchUpdateNonstrict(entityList).
+     * Other specifications are same as queryDelete(cb).
      * @param cb The condition-bean of WhiteDelimiter. (NotNull)
      * @param opLambda The callback for option of delete for varying requests. (NotNull)
      * @return The deleted count.
